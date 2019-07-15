3582d9a934539fa29d9337ed0feee8896cc73761
adeolaonads <43695498+adeolaonads@users.noreply.github.com>
2019-07-15 09:56:52 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class InstagramBot:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox()
+
+
+    def login(self):
+        bot = self.bot
+        bot.get('https://www.instagram.com/accounts/login/?source=auth_switcher/')
+        time.sleep(3)
+        email = bot.find_element_by_name('username')
+        password = bot.find_element_by_name('password')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(3)
+    
+    def like_post(self,hashtag):
+        bot = self.bot
+        bot.get('https://www.instagram.com/' + hashtag + '/')
+
+
+ade = InstagramBot('Your email here', 'Your password here')
+ade.login()
+ade.like_post('instablog9ja')
